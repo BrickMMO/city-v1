a7ebf0190a9ba713e408f2a16e5cc077373ca874
Adam Thomas <thomasadam83@hotmail.com>
2025-11-25 23:34:48 -0500

    Update events/details.php

--- events/details.php
+++ events/details.php
@@ -22,58 +22,53 @@ $record = mysqli_fetch_assoc($result);
 
 ?>
 
-<main>   
-    
-    <div class="w3-center">
-        <h1><?=$record['name']?></h1>
-    </div>
 
-    <hr>
+<div class="w3-center">
+    <h1><?=$record['name']?></h1>
+</div>
 
+<hr>
 
-    <?php if($record['banner']): ?>
-        <img src="<?=$record['banner']?>" class="w3-image" style="width: 100%;">
-    <?php endif; ?>
+<?php if($record['banner']): ?>
+    <img src="<?=$record['banner']?>" class="w3-image" style="width: 100%;">
+<?php endif; ?>
 
-    <p><?=nl2br($record['description'])?></p>
+<p><?=nl2br($record['description'])?></p>
 
-    <?php if(!$record['registration']): ?>
-        <p>Note: <span class="w3-bold">No registration required, just show up!</span></p>
-    <?php endif; ?>
+<?php if(!$record['registration']): ?>
+    <p>Note: <span class="w3-bold">No registration required, just show up!</span></p>
+<?php endif; ?>
 
-    <hr>
+<hr>
 
-    <p>
-        Date: <span class="w3-bold"><?=date_to_format($record['starts_at'], 'FULL')?></span>
-        <br>
-        Location: <span class="w3-bold"><?=$record['location']?></span>
-    </p>
+<p>
+    Date: <span class="w3-bold"><?=date_to_format($record['starts_at'], 'FULL')?></span>
+    <br>
+    Location: <span class="w3-bold"><?=$record['location']?></span>
+</p>
 
-    <hr>
-    
-    <?php if($record['registration']): ?>
-        <a href="<?=$record['registration']?>" class="w3-button w3-white w3-border">
-            <i class="fa-solid fa-pen fa-padding-right"></i> Register
-        </a>
-        <hr>
-    <?php endif; ?>
+<hr>
 
-    <?php if($record['online']): ?>
-        <a href="<?=$record['online']?>" class="w3-button w3-white w3-border">
-            <i class="fa-solid fa-globe fa-padding-right"></i> Join Online
-        </a>
-    <?php endif; ?>
-    <a href="/list" class="w3-button w3-white w3-border">
-        <i class="fa-solid fa-caret-left fa-padding-right"></i>
-        Back to Event List
+<?php if($record['registration']): ?>
+    <a href="<?=$record['registration']?>" class="w3-button w3-white w3-border">
+        <i class="fa-solid fa-pen fa-padding-right"></i> Register
     </a>
-    <a href="/calendar" class="w3-button w3-white w3-border">
-        <i class="fa-solid fa-calendar fa-padding-right"></i>
-        Back to Calendar View
-    </a>
+    <hr>
+<?php endif; ?>
 
-            
-</main>
+<?php if($record['online']): ?>
+    <a href="<?=$record['online']?>" class="w3-button w3-white w3-border">
+        <i class="fa-solid fa-globe fa-padding-right"></i> Join Online
+    </a>
+<?php endif; ?>
+<a href="/list" class="w3-button w3-white w3-border">
+    <i class="fa-solid fa-caret-left fa-padding-right"></i>
+    Back to Event List
+</a>
+<a href="/calendar" class="w3-button w3-white w3-border">
+    <i class="fa-solid fa-calendar fa-padding-right"></i>
+    Back to Calendar View
+</a>
 
 <?php
 
